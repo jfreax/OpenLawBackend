17ca9dee0ed16e08320078e9eed1eb5dd2dfa105
Jens Dieskau <jfreax@googlemail.com>
2013-07-10 21:16:32 +0200

    Update db schemata.

--- schema.sql
+++ schema.sql
@@ -7,13 +7,24 @@ create table Laws (
 );
 
 
-drop table if exists Law_Text;
-create table Law_Text (
-  id integer,
+drop table if exists Law_Heads;
+create table Law_Heads (
+  id integer not null,
   law_id integer not null,
-  text text not null,
-
+  headline text not null,
+  depth integer not null,
   CONSTRAINT pk_law_text_id PRIMARY KEY (id, law_id),
   FOREIGN KEY (law_id) REFERENCES Laws(id)
+);
+ 
+
+drop table if exists Law_Texts;
+create table Law_Texts (
+  id integer,
+  head_id integer not null,
+  text text not null,
+
+  CONSTRAINT pk_law_head_id PRIMARY KEY (id, head_id),
+  FOREIGN KEY (head_id) REFERENCES Law_Heads(id)
 );
  
