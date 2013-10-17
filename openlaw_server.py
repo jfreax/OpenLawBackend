d710662a49a5f5b3b78dc3b285f86efdc86792d6
Jens Dieskau <jens@dieskau.pm>
2013-10-17 21:42:12 +0200

    Intend with 4 spaces

--- openlaw_server.py
+++ openlaw_server.py
@@ -36,11 +36,11 @@ AUTH_TOKEN_STRING = getAuthToken();
 # Tracking method #
 ###################
 def do_piwik(ip, url, title):
-	piwiktracker.set_ip(ip)
-	piwiktracker.set_token_auth(AUTH_TOKEN_STRING)
-	piwiktracker.set_url("http://"+url)
-	title = title.encode('ascii',"ignore")
-	piwiktracker.do_track_page_view(title)
+    piwiktracker.set_ip(ip)
+    piwiktracker.set_token_auth(AUTH_TOKEN_STRING)
+    piwiktracker.set_url("http://"+url)
+    title = title.encode('ascii',"ignore")
+    piwiktracker.do_track_page_view(title)
 
 # Decorations #
 ###############
@@ -65,81 +65,81 @@ app.config.from_object(__name__)
 
 @app.before_request
 def before_request():
-	g.db = connect_db()
+    g.db = connect_db()
 
 @app.teardown_request
 def teardown_request(exception):
-	db = getattr(g, 'db', None)
-	if db is not None:
-		db.close()
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
 
 @app.route('/laws')
 @support_jsonp
 def show_all_laws():
-	cur = g.db.execute('select slug, short_name, long_name from Laws')
-	entries = [dict(slug=row[0], short=row[1], long=row[2]) for row in cur.fetchall()]
+    cur = g.db.execute('select slug, short_name, long_name from Laws')
+    entries = [dict(slug=row[0], short=row[1], long=row[2]) for row in cur.fetchall()]
 
-	thread.start_new_thread(do_piwik, (request.remote_addr, headers["SERVER_NAME"]+"/laws", "laws"))
-	
-	response = Response(response = render_template('laws', laws=entries),
-			status = 200,
-			mimetype = "application/json; charset=utf-8")
-	return response
+    thread.start_new_thread(do_piwik, (request.remote_addr, headers["SERVER_NAME"]+"/laws", "laws"))
+    
+    response = Response(response = render_template('laws', laws=entries),
+            status = 200,
+            mimetype = "application/json; charset=utf-8")
+    return response
 
 @app.route('/<slug>')
 def show_head_of_law(slug):
-	cur = g.db.execute('\
-		select \
-			Law_Heads.headline, \
-			Law_Heads.depth \
-		from \
-			Laws, \
-			Law_Heads \
-		where \
-			Law_Heads.law_id == Laws.id \
-			and Laws.slug == ?', [slug])
-	entries = [dict(headline=row[0], depth=row[1]) for row in cur.fetchall()]
+    cur = g.db.execute('\
+        select \
+            Law_Heads.headline, \
+            Law_Heads.depth \
+        from \
+            Laws, \
+            Law_Heads \
+        where \
+            Law_Heads.law_id == Laws.id \
+            and Laws.slug == ?', [slug])
+    entries = [dict(headline=row[0], depth=row[1]) for row in cur.fetchall()]
 
-	cur = g.db.execute('\
-		select \
-			Laws.long_name \
-		from \
-			Laws \
-		where \
-			Laws.slug == ?', [slug])
-	law_name = cur.fetchall()[0][0]
-	thread.start_new_thread(do_piwik,
-		(request.remote_addr, headers["SERVER_NAME"]+"/"+slug, u"%s - %s" % (slug, law_name.replace(u'\\', u'')))
-	)
+    cur = g.db.execute('\
+        select \
+            Laws.long_name \
+        from \
+            Laws \
+        where \
+            Laws.slug == ?', [slug])
+    law_name = cur.fetchall()[0][0]
+    thread.start_new_thread(do_piwik,
+        (request.remote_addr, headers["SERVER_NAME"]+"/"+slug, u"%s - %s" % (slug, law_name.replace(u'\\', u'')))
+    )
 
-	return render_template('heads', heads=entries)
+    return render_template('heads', heads=entries)
 
 @app.route('/<slug>/<int:i>')
 def show_law_text(slug, i):
-	cur = g.db.execute('\
-		select \
-			Law_Texts.text, \
-			Law_Heads.headline \
-		from \
-			Laws, \
-			Law_Texts, \
-			Law_Heads \
-		where \
-			Law_Texts.law_id == Laws.id and \
-			Laws.slug == ? and \
-			Law_Heads.law_id == ? and \
-			Law_Texts.head_id == ?', [slug, i, i])
-	fetchs = cur.fetchall()
-	text = fetchs[0][0]
-	headline = fetchs[0][1]
+    cur = g.db.execute('\
+        select \
+            Law_Texts.text, \
+            Law_Heads.headline \
+        from \
+            Laws, \
+            Law_Texts, \
+            Law_Heads \
+        where \
+            Law_Texts.law_id == Laws.id and \
+            Laws.slug == ? and \
+            Law_Heads.law_id == ? and \
+            Law_Texts.head_id == ?', [slug, i, i])
+    fetchs = cur.fetchall()
+    text = fetchs[0][0]
+    headline = fetchs[0][1]
 
-	thread.start_new_thread(do_piwik, 
-		(request.remote_addr, headers["SERVER_NAME"]+"/"+slug+"/"+str(i), u"%s - %s" % (slug, headline.replace(u'\\', u'')))
-	)
+    thread.start_new_thread(do_piwik, 
+        (request.remote_addr, headers["SERVER_NAME"]+"/"+slug+"/"+str(i), u"%s - %s" % (slug, headline.replace(u'\\', u'')))
+    )
 
-	return text
+    return text
 
 if __name__ == '__main__':
-        app.debug = True
-	app.run()
+    app.debug = True
+    app.run()
 
