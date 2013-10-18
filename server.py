97ccbb56f6ef6d3f3340adef2091d7ed4d7a133f
Jens Dieskau <jens@dieskau.pm>
2013-10-18 02:33:22 +0200

    Bad request as json

--- server.py
+++ server.py
@@ -159,6 +159,10 @@ def show_law_text(slug, i):
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
+  
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
 
 if __name__ == '__main__':
     app.debug = True
